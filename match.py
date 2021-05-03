c76e68ea8bcaa373e985f04e498707d276bf9189
Eagle Code <tomoki.takaba@outlook.jp>
2021-05-03 12:28:15 +0900

    no message

--- match.py
+++ match.py
@@ -20,6 +20,7 @@ r_yaw =math.radians(0)
 r_pitch =math.radians(0)
 view_w=math.radians(36.17615)
 view_h=math.radians(25.98925)
+range=0.35
 # 画像１
 img1 = cv2.imread("test_1.jpg")
 # 画像２
@@ -46,16 +47,14 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 
 # 特徴量ベクトル同士をBrute-Force＆KNNでマッチング
 match = bf.knnMatch(des1,des2,k=2)
-ratio = 0.5
+ratio = 0.25
 matches=[]
 for m, n in match:
     if m.distance < ratio * n.distance:
         matches.append([m])
 
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, flags=2)
-cv2.imshow("test",img3)
+
 matches=np.array(matches).flatten().tolist()
-cv2.imwrite('img3.jpg', img3)
 
 def convert_3d(data):
     d=375/math.tan(view_w)
@@ -63,25 +62,53 @@ def convert_3d(data):
     y=d
     z=((250-data[1]))
     
-    return round(x,4),round(y,4),round(z,4)
+    return round(x,2),round(y,2),round(z,2)
+
+
+def each_delta(a,b):
+    A=np.array([
+        [sum(a[:2]),sum(a[1:])],
+        [sum(b[:2]),sum(b[1:])]
+    ])
+    R=np.array([
+        [a[0],a[1],a[2]],
+        [b[0],b[1],b[2]],
+    ])
+    alufa=np.cross(A[:,0],A[:,1])
+    Right=np.matrix([
+        [np.cross(A[:,1],R[:,0])-alufa,np.cross(A[:,1],R[:,0])+np.cross(A[:,0],R[:,0]),np.cross(A[:,0],R[:,0])],
+        [np.cross(A[:,1],R[:,1]),np.cross(A[:,1],R[:,1])+np.cross(A[:,0],R[:,1])-alufa,np.cross(A[:,0],R[:,1])],
+        [np.cross(A[:,1],R[:,2]),np.cross(A[:,1],R[:,2])+np.cross(A[:,0],R[:,2]),np.cross(A[:,0],R[:,2])-alufa]
+    ])
+    Left=np.matrix([
+        [-1*(-0.6*np.cross(R[:,0],A[:,1])+0*np.cross(A[:,0],R[:,0])+0.6*alufa)],
+        [-1*(-0.6*np.cross(R[:,1],A[:,1])+0*np.cross(A[:,0],R[:,1])-0*alufa)],
+        [-1*(-0.6*np.cross(R[:,2],A[:,1])+0*np.cross(A[:,0],R[:,2])-0*alufa)],
+    ])
+    coe_delta = (np.linalg.pinv(Right)*Left).reshape(-1,).tolist()
+    return (coe_delta[0][0],coe_delta[0][1],coe_delta[0][2])
 
 def convert(data):
     data_converted=[]
     for loc in data:
         data_converted.append(convert_3d(loc))
+    x,y,z=each_delta(data_converted[0],data_converted[1])
     A=np.matrix([
         [-1*sum(data_converted[1][:2]),sum(data_converted[0][:2])],
         [-1*sum(data_converted[1][1:]),sum(data_converted[0][1:])],
     ])
-    Y=np.matrix([
-        [-0.6],
-        [0],
-    ])
-    coe = np.linalg.solve(A,Y).reshape(-1,).tolist()
-    if data_converted[1][1]*abs(coe[0][1])<210:
-        x=1
+    if abs(x**2+y**2+z)<range:
+        Y=np.matrix([
+            [-0.6+x+y],
+            [0+y+z],
+        ])
     else:
-        print([i-l for i,l in zip((data_converted[1][0]*coe[0][1],data_converted[1][1]*coe[0][1],data_converted[1][2]*coe[0][1]),(data_converted[1][0]*coe[0][0],data_converted[1][1]*coe[0][0],data_converted[1][2]*coe[0][0]))])
+        Y=np.matrix([
+            [-0.6],
+            [0],
+        ])
+    coe = np.linalg.solve(A,Y).reshape(-1,).tolist()
+    
     return data_converted[1][0]*coe[0][0],data_converted[1][1]*coe[0][0],data_converted[1][2]*coe[0][0]
 
 def collect(data):
@@ -97,6 +124,5 @@ def collect(data):
     ax.view_init(elev=0, azim=0)
     plt.show()
     
-
 
 collect(matches)
